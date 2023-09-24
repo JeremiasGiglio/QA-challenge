b915f40b06c6129b71d65ffd048515c823c31b20
JeremiasGiglio <jeremiasiglio@gmail.com>
2023-09-24 14:05:35 -0300

    Api tests

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+module.exports = defineConfig({
+  env: {
+    url: 'https://www.pokemon.com/us'
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
